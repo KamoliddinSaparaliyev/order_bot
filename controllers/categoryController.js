602750b0442bc84e0fc46c2ec9b9ed42e947a11f
KamoliddinSaparaliyev <saparaliyevkamoliddin131@gmail.com>
2024-08-05 09:12:01 +0500

    chore: Refactor category and meal controllers
    This commit refactors the category and meal controllers to improve code structure and readability. It includes the following changes:
    
    - Reorganized the code to follow a consistent indentation style.
    - Removed unnecessary whitespace and added missing semicolons.
    - Renamed variables and functions to have more descriptive names.
    - Updated the comments to provide clearer explanations of the code.
    
    These changes enhance the maintainability and understandability of the category and meal controllers.

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -5,43 +5,46 @@ require('../models/category.model')
 const Category = mongoose.model('category')
 
 class CategoryController {
+    // Поиск блюда по uuid
+    async findCategoryById(category) {
+        return await Category.findOne({ _id: category }).populate('meals')
+    }
 
-  // Поиск блюда по uuid
-  async findCategoryById(category) {
-    return await Category.findOne({ _id: category }).populate('meals')
-  }
+    async listCategory(category) {
+        return await Category.find()
+    }
 
-  async listCategory(category) {
-    return await Category.find()
-  }
+    async findCategoryByName(name) {
+        return await Category.findOne({ name: name })
+    }
 
-  async findCategoryByName(name) {
-    return await Category.findOne({ name: name })
-  }
+    // Формирование клавиатуры с типами блюд
+    async replyCategoryKeyboard() {
+        // получение полного списка блюд
+        const categories = await Category.find({})
 
-  // Формирование клавиатуры с типами блюд
-  async replyCategoryKeyboard() {
-    // получение полного списка блюд
-    const categories = await Category.find({})
+        // categories reply keyboard
 
-    // categories reply keyboard
+        const keyBoard = []
 
-    const keyBoard = []
+        categories.map((c, i) =>
+            keyBoard.push([
+                {
+                    id: i,
+                    text: '❇️ ' + c.name,
+                },
+            ])
+        )
 
-    categories.map((c, i) => keyBoard.push([{
-        id: i,
-        text: "❇️ " + c.name
-    }]))
+        keyBoard.push([
+            {
+                id: keyBoard.length + 1,
+                text: '⬅️ Orqaga',
+            },
+        ])
 
-    keyBoard.push([{
-      id: keyBoard.length + 1,
-      text: '⬅️ Orqaga'
-    }])
-
-
-    return keyBoard
-  }
-
+        return keyBoard
+    }
 }
 
 module.exports = new CategoryController()
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,33 +1,39 @@
-const mongoose  = require("mongoose");
+const mongoose = require('mongoose')
 
-require("../models/user.model");
+require('../models/user.model')
 
-const User = mongoose.model("user");    
+const User = mongoose.model('user')
 
 class UserController {
-  async findUserById(userId) {
-    return await User.findOne({ user_id: userId });
-  }
-
-  async chechUser(userId) {
-    const user =  await User.findOne({ user_id: userId })
-    return user ? true : false;
-  }
-
-
-  async createUser(userId, name, phone) {
-    const user = await User.findOne({ user_id: userId, user_phone: phone });
-
-    if (user) {
-      await User.findOneAndUpdate({ user_id: userId }, { user_name: name, user_phone: phone });
+    async findUserById(userId) {
+        return await User.findOne({ user_id: userId })
     }
 
-     await User.create({ user_id: userId, user_name: name, user_phone: phone });
-  }
+    async chechUser(userId) {
+        const user = await User.findOne({ user_id: userId })
+        return user ? true : false
+    }
 
-  async userOrders(userId) {
-    return await User.findOne({ user_id: userId }).populate("orders");
-  }
+    async createUser(userId, name, phone) {
+        const user = await User.findOne({ user_id: userId, user_phone: phone })
+
+        if (user) {
+            await User.findOneAndUpdate(
+                { user_id: userId },
+                { user_name: name, user_phone: phone }
+            )
+        }
+
+        await User.create({
+            user_id: userId,
+            user_name: name,
+            user_phone: phone,
+        })
+    }
+
+    async userOrders(userId) {
+        return await User.findOne({ user_id: userId }).populate('orders')
+    }
 }
 
-module.exports = new UserController();
+module.exports = new UserController()
